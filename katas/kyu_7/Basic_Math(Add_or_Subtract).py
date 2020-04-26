1f84245d9dd35937552dfbbc5c767fe3460c744c
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-26 11:59:03 +0530

    Create Basic_Math(Add_or_Subtract).py

---
+++ katas/kyu_7/Basic_Math(Add_or_Subtract).py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/5809b62808ad92e31b000031/
+
+'''
+Instructions :
+
+In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+Note: the input will not be empty.
+
+Examples
+"1plus2plus3plus4"  --> "10"
+"1plus2plus3minus4" -->  "2"
+'''
+
+
+def calculate(s):
+    return str(eval((s.replace("plus","+")).replace("minus","-")))
