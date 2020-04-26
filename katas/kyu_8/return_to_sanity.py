fb2a614f78e0d1fbbba1df247cf95d363cab8d22
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-26 08:58:56 +0530

    Create return_to_sanity.py

---
+++ katas/kyu_8/return_to_sanity.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/514a7ac1a33775cbb500001e
+
+# Details : This function should return an object, but it's not doing what's intended. What's wrong?
+
+
+def mystery():
+    results = {'sanity': 'Hello'}
+    return results
