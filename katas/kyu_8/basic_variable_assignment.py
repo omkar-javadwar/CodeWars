5e4f4705debecdf4343df5d50ae4bce4b0a98254
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-26 08:56:29 +0530

    Update basic_variable_assignment.py

--- katas/kyu_8/basic_variable_assignment.py
+++ katas/kyu_8/basic_variable_assignment.py
@@ -1,5 +1,5 @@
 # https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/python
-# Basic variable assignment
+
 # Instructions : This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
 
 
