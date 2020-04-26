46b424d50113370c70186ed6e8ae3df08670e0a6
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-26 12:12:20 +0530

    Create 5_without_numbers!!.py

---
+++ katas/kyu_8/5_without_numbers!!.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/59441520102eaa25260000bf/
+
+'''
+Instructions :
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+'''
+
+
+def unusual_five():
+    str = len("hello")
+    return int(str)
