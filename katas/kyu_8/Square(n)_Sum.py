f6ee05093a23e58d506201bfc9fb5403d031838e
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-26 09:05:40 +0530

    Create Square(n)_Sum.py

---
+++ katas/kyu_8/Square(n)_Sum.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/515e271a311df0350d00000f
+
+'''
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+'''
+
+
+def square_sum(numbers):
+    x = []
+    for i in numbers:
+        x.append(i ** 2)
+    return sum(x)
