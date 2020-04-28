a11681ae0c1f704081ef89db0a4548a183e54c5e
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-28 10:19:05 +0530

    Create Find_the_missing_letter.py

---
+++ katas/kyu_6/Find_the_missing_letter.py
@@ -0,0 +1,32 @@
+# 
+
+'''
+Instructions :
+
+#Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+["a","b","c","d","f"] -> "e"
+["O","Q","R","S"] -> "P"
+(Use the English alphabet with 26 letters!)
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+'''
+
+
+import string 
+def find_missing_letter(chars):
+    charset = string.ascii_lowercase if chars[0] >= 'a' else string.ascii_uppercase
+    for letter in charset[charset.index(chars[0]):]:
+        if letter not in chars:
+            return letter[0]
