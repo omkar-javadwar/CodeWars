268e3039e178443d709716b63b852c57fdf7c9a3
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-28 08:37:30 +0530

    Create Replace_With_Alphabet_Position.py

---
+++ katas/kyu_6/Replace_With_Alphabet_Position.py
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/546f922b54af40e1e90001da/python
+
+'''
+Instructions :
+
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+  alphabet_position("The sunset sets at twelve o' clock.")
+  Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+'''
+
+
+def alphabet_position(text):
+    lst = []
+    dict = {'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4, 'g': 7, 'f': 6, 'i': 9, 'h': 8, 'k': 11, 'j': 10, 'm': 13, 'l': 12, 'o': 15, 'n': 14, 'q': 17, 'p': 16, 's': 19, 'r': 18, 'u': 21, 't': 20, 'w': 23, 'v': 22, 'y': 25, 'x': 24, 'z': 26}
+    for i in text.lower():
+        if i in dict:
+            lst.append(dict[i])
+    return ' '.join(str(i) for i in lst)
