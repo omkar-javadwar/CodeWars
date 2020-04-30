d34a694ba645ea2b4de183a280c5516964b187ac
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-04-30 18:12:08 +0530

    Create Regex_Password_Validation.py

---
+++ katas/kyu_5/Regex_Password_Validation.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/52e1476c8147a7547a000811/
+
+'''
+Instructions :
+
+You need to write regex that will validate a password to make sure it meets the following criteria:
+
+At least six characters long
+contains a lowercase letter
+contains an uppercase letter
+contains a number
+Valid passwords will only be alphanumeric characters.
+
+'''
+
+
+regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$"
