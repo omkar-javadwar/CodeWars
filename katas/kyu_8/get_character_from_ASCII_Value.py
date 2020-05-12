cd27ae9957d3f0e0ce71473240e8fb2338d702f5
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-05-12 18:43:45 +0530

    Create get_character_from_ASCII_Value.py

---
+++ katas/kyu_8/get_character_from_ASCII_Value.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/python
+
+'''
+Instructions :
+
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65) # => 'A'
+For ASCII table, you can refer to http://www.asciitable.com/
+'''
+
+
+def get_char(c):
+  return chr(c)
