ee3f5b772d6d4832e5521cd9ffaf1353fb72adec
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-05-12 18:39:36 +0530

    Create get_ascii_value_of_character.py

---
+++ katas/kyu_8/get_ascii_value_of_character.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/python
+
+'''
+
+Instructions :
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+'''
+
+
+def get_ascii(c):
+    return ord(c)
