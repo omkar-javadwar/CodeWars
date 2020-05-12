63bc95c28d5769133ef07cbccb8a72353e107372
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-05-13 00:15:51 +0530

    Create Check_for_prime_numbers.py

---
+++ katas/kyu_7/Check_for_prime_numbers.py
@@ -0,0 +1,24 @@
+# https://www.codewars.com/kata/53daa9e5af55c184db00025f/train/python
+
+'''
+Instructions :
+
+In this kata you will create a function to check a non-negative input to see if it is a prime number.
+
+The function will take in a number and will return True if it is a prime number and False if it is not.
+
+A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Examples
+isPrime(0) is false
+isPrime(1) is false
+isPrime(2) is true
+isPrime(11) is true
+isPrime(12) is false
+'''
+
+
+from math import sqrt
+
+def is_prime(n):
+  return n > 1 and all(n % d for d in range(2, int(sqrt(n)) + 1))
