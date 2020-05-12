23bd6a6020ce43d51938b8ea893c00ecfcb801c3
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-05-12 18:53:21 +0530

    Create Letter_to_ASCII.py

---
+++ katas/beta/Letter_to_ASCII.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/58dc15eed2ba027579000175/train/python
+
+'''
+Instructions :
+
+This kata is very simple.
+
+You will be given a character and you will convert it to its corresponding number.
+
+Ex. 'a' = 97
+
+Here is a link to an ASCII table: http://www.asciitable.com/
+'''
+
+
+def charToASCII(c):
+    return ord(c)
