c172d802dd8578c0c114f6fddb15aa2a9c93cc42
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-06-07 11:55:07 +0530

    Create Simple_Sentences.py

---
+++ katas/kyu_6/Simple_Sentences.py
@@ -0,0 +1,26 @@
+# https://www.codewars.com/kata/5297bf69649be865e6000922/train/python
+
+'''
+Instructions:
+
+Implement a function, so it will produce a sentence out of the given parts.
+
+Array of parts could contain:
+
+words;
+commas in the middle;
+multiple periods at the end.
+Sentence making rules:
+
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+Example:
+
+makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.'
+
+'''
+
+
+def make_sentences(parts):
+    return ' '.join(parts).replace(' , ',', ').replace(' .','') + '.'
