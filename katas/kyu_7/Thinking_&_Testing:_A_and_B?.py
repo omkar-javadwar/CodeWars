2700b8f8e32a7c930235661a6040cc977c5f3cd8
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-06-14 15:55:31 +0530

    Create Thinking_&_Testing:_A_and_B?.py

---
+++ katas/kyu_7/Thinking_&_Testing:_A_and_B?.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/python
+
+'''
+Instructions:
+
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+'''
+
+
+def testit (a, b):
+    # Good Luck
+    return a | b # Hmm is that right?
