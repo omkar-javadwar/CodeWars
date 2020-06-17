9427cd72fbc24bac6e5b503cdaba685797798dd4
Omkar Javadwar <43721555+omkar-javadwar@users.noreply.github.com>
2020-06-17 12:55:06 +0530

    Create Smallest_unused_ID.py

---
+++ katas/kyu_8/Smallest_unused_ID.py
@@ -0,0 +1,21 @@
+# https://www.codewars.com/kata/55eea63119278d571d00006a/train/python
+
+'''
+Instructions:
+
+Hey awesome programmer!
+
+You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+Go on and code some pure awesomeness!
+
+'''
+
+def next_id(arr):
+    for i in range(len(arr)+1):
+        if i not in arr:
+            return i
